d843ef92da5a14f54aea893188d995212aebd3cf
Yinkl7 <381964421@qq.com>
2024-07-15 12:42:35 +0800

    初始化项目

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home-view">home-view</div>
+</template>
+
+<style lang="less" scoped>
+.home-view {
+  // background-color: yellow;
+}
+</style>
