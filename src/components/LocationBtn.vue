958c9bb6ae3156306718cefdcabf965eb38897ea
Yinkl7 <381964421@qq.com>
2024-07-22 20:30:56 +0800

    增加定位

--- src/components/LocationBtn.vue
+++ src/components/LocationBtn.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getMapAnchorList } from '@/js/api'
+import { onMounted, ref } from 'vue'
+
+onMounted(() => {
+  init()
+})
+
+const mapAnchorList = ref<any[]>([])
+
+async function init() {
+  let res = await getMapAnchorList()
+  // console.log('===== ', res.data)
+  mapAnchorList.value = res.data
+}
+</script>
 
 <template>
-  <div class="location-btn"></div>
+  <div class="location-btn">
+    <div class="location-icon"></div>
+    <div class="location-content">
+      <div class="location-title">快速定位</div>
+      <div class="content-areas">
+        <div class="area-item" v-for="item in mapAnchorList" :key="item.id">
+          <div class="area-parent">
+            <div class="parent-icon"></div>
+            <div class="parent-name">{{ item.name }}</div>
+          </div>
+          <div class="area-child" v-for="child in item.children" :key="child.id">{{ child.name }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
@@ -14,5 +43,81 @@
   background-color: rgba(50, 57, 71, 0.8);
   cursor: pointer;
   border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    .location-content {
+      visibility: visible;
+    }
+  }
+  .location-content {
+    position: absolute;
+    left: 60px;
+    top: 0;
+    background-color: #3b4354;
+    border-radius: 12px;
+    visibility: hidden;
+    transition: 0.5s all;
+    user-select: none;
+    width: 192px;
+    .location-title {
+      font-size: 16px;
+      color: #d3bc8e;
+      padding: 12px;
+      border-bottom: 1px solid rgba(86, 97, 120, 0.2);
+    }
+
+    .content-areas {
+      height: 500px;
+      overflow-y: auto;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+      .area-item {
+        .area-child {
+          display: flex;
+          align-items: center;
+          padding-left: 32px;
+          height: 48px;
+          color: hsla(39, 34%, 89%, 0.75);
+          &:hover {
+            color: #ece5d8;
+          }
+        }
+        .area-parent {
+          display: flex;
+          align-items: center;
+          height: 48px;
+          padding-left: 15px;
+          &:hover {
+            .parent-name {
+              color: #ece5db;
+            }
+            .parent-icon {
+              background-image: url('../assets/images/ui/location-icon-h.png');
+            }
+          }
+          .parent-icon {
+            width: 12px;
+            height: 12px;
+            background-image: url('../assets/images/ui/location-icon-n.png');
+            background-size: cover;
+            margin-right: 5px;
+          }
+          .parent-name {
+            font-size: 14px;
+            color: #d3bc8e;
+          }
+        }
+      }
+    }
+  }
+  .location-icon {
+    width: 24px;
+    height: 24px;
+    background-size: cover;
+    background-image: url('../assets/images/ui/location-btn.png');
+  }
 }
 </style>
--- src/js/mock/index.ts
+++ src/js/mock/index.ts
@@ -1,6 +1,7 @@
 import mock from 'mockjs'
 import { MainHost } from '../api/constants'
 import { LabelTreeData } from './label-tree-data'
+import { MapAnchorList } from './map-anchor-list'
 
 function mockLabelTree() {
   mock.mock(new RegExp(`${MainHost}/label/tree($|\\?.*)`), {
@@ -10,6 +11,15 @@ function mockLabelTree() {
   })
 }
 
+function mockMapAnchorList() {
+  mock.mock(new RegExp(`${MainHost}/map_anchor/list($|\\?.*)`), {
+    code: 0,
+    data: MapAnchorList,
+    message: '成功'
+  })
+}
+
 export function mockAllData() {
   mockLabelTree()
+  mockMapAnchorList()
 }
